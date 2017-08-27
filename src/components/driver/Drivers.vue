8f77ccca844231c3e40a797371481e32590c82c5
Minh Phuong <phuongdm1987@gmail.com>
2017-08-27 23:18:28 +0700

    get all driver

--- src/components/driver/Drivers.vue
+++ src/components/driver/Drivers.vue
@@ -1,26 +1,33 @@
 <template>
-  <div>
-    <h4>Drivers</h4>
+  <v-card>
+    <v-card-title>
+      Drivers
+      <v-spacer></v-spacer>
+      <v-text-field
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        v-model="search"
+      ></v-text-field>
+    </v-card-title>
     <v-data-table
       v-bind:headers="headers"
-      v-bind:items="items"
+      v-bind:items="drivers"
       v-bind:search="search"
-      v-bind:pagination.sync="pagination"
-      :total-items="totalItems"
-      :loading="loading"
-      class="elevation-1"
     >
-      <template slot="headerCell" scope="props">
+      <template slot="items" scope="props">
         <span v-tooltip:bottom="{ 'html': props.header.text }">
           {{ props.header.text }}
         </span>
       </template>
       <template slot="items" scope="props">
         <td>
-          {{ props.item.name }} <br>
+          {{ props.item.name }} [{{ props.item.rating_count }}/{{ props.item.rating_avg }}]<br>
           {{ props.item.phone }} <br>
           {{ props.item.email }}
         </td>
+        <td>{{ props.item.address }}</td>
         <td>
           <span class="left">All:</span>
           <span class="right">{{ props.item.shipment_count }}</span>
@@ -52,23 +59,23 @@
           <span class="right">{{ props.item.expired_at }}</span>
         </td>
         <td>
-          <v-chip small v-tooltip:left="{ html: 'Verify' }" :class="{ orange : props.item.is_verify }" class="white--text">
-            <v-icon right>star</v-icon>
-          </v-chip>
-          <v-chip v-tooltip:left="{ html: 'Active' }" small :class="{ green : props.item.is_active }" class="white--text">
-            <v-icon right>check_circle</v-icon>
-          </v-chip>
+          <v-btn icon v-tooltip:left="{ html: 'Verify' }" :class="{ 'orange--text' : props.item.is_verify }">
+            <v-icon>star</v-icon>
+          </v-btn>
+          <v-btn icon v-tooltip:left="{ html: 'Online' }" :class="{ 'teal--text' : props.item.is_online }">
+            <v-icon>face</v-icon>
+          </v-btn>
           <br>
-          <v-chip small v-tooltip:left="{ html: 'Online' }" :class="{ teal : props.item.is_online }" class="white--text">
-            <v-icon right>face</v-icon>
-          </v-chip>
-          <v-chip small v-tooltip:left="{ html: 'In app' }" :class="{ primary : props.item.is_inapp }" class="white--text">
-            <v-icon right>input</v-icon>
-          </v-chip>
+          <v-btn icon v-tooltip:left="{ html: 'In app' }" :class="{ 'primary' : props.item.is_inapp }">
+            <v-icon>input</v-icon>
+          </v-btn>
+          <v-btn icon v-tooltip:left="{ html: 'Detail' }" class="warning">
+            <v-icon>info_outline</v-icon>
+          </v-btn>
         </td>
       </template>
     </v-data-table>
-  </div>
+  </v-card>
 </template>
 
 <script>
@@ -78,17 +85,15 @@ export default {
   data () {
     return {
       search: '',
-      totalItems: 0,
-      items: [],
       loading: true,
-      pagination: {},
       headers: [
         {
           text: 'Drivers listing',
           align: 'left',
           sortable: false,
-          value: 'name'
+          value: 'phone'
         },
+        { text: 'Address', value: 'address' },
         { text: 'Shipment', value: 'shipment_count' },
         { text: 'Balance', value: 'balance' },
         { text: 'Date', value: 'packaged_at' },
@@ -102,24 +107,12 @@ export default {
   methods: {
     ...mapActions('driver', ['getDrivers'])
   },
-  watch: {
-    drivers () {
-      if (this.drivers) {
-        if (this.drivers.data) {
-          this.items = this.drivers.data
-        }
-        if (this.drivers.meta) {
-          this.totalItems = this.drivers.meta.pagination.total
-        }
-      }
-    }
-  },
   mounted () {
-    // if (this.drivers.length <= 0) {
-    this.loading = true
-    this.getDrivers()
-    this.loading = false
-    // }
+    if (this.drivers.length <= 0) {
+      this.loading = true
+      this.getDrivers()
+      this.loading = false
+    }
   }
 }
 </script>
